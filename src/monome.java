259ee8bd121bb27ddeaa7e33cdb7bb505f3cdc22
jpeacockinc <jpeacockinc@Gmail.com>
2013-03-30 04:30:39 -0400

    Background w/ overlay but no camera

--- src/monome.java
+++ src/monome.java
@@ -1,12 +1,24 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class monome {
 	
-	public static void main(String[] args){	
+	public static void main(String[] args) {		
+		JFrame frame = new JFrame("Transparent Window");
+		ui bg = new ui(frame);
+		bg.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel label = new JLabel("This is a label");
+		bg.add("South",label);
+		frame.getContentPane().add("Center",bg);
+		frame.pack();
+		frame.setSize(800,800);
+		frame.setVisible(true); 
+	        
 		
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			ui test = new ui();
-	        public void run() {
-	        	test.dOverlayCamera();
-	        }
-		});
 	}
 }
